7fdf3d0dfddcff791ea3b050ac3c93d67d1638cf
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-08 12:55:53 -0200

    redux for cars

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -2,20 +2,22 @@ import React, {useState} from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 
 function Header() {
     const[burgerStatus, setBugerStatus] = useState(false)
+    const cars = useSelector(selectCars)
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg" alt=""/>
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car,index)=>(
+                    <a key={index} href="#">{car}</a>
+                ))}
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
@@ -26,14 +28,14 @@ function Header() {
                 <CloseWrapper>
                     <CustomClose onClick={()=>setBugerStatus(false)}/>
                 </CloseWrapper>
+                {cars && cars.map((car,index)=>(
+                    <li key={index}><a href="#">{car}</a></li>
+                ))}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-In</a></li>
                 <li><a href="#">Cybertruck</a></li>
                 <li><a href="#">Roadaster</a></li>
-                <li><a href="#">Existing Inventory</a></li>
-                <li><a href="#">Existing Inventory</a></li>
-                <li><a href="#">Existing Inventory</a></li>
             </BurgerNav>
         </Container>
     )
