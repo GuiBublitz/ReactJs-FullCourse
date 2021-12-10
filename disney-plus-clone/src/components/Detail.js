0082ed7095a9df3afe8352d95da932e13a271cf3
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-10 14:48:32 -0200

    Detail page

--- disney-plus-clone/src/components/Detail.js
+++ disney-plus-clone/src/components/Detail.js
@@ -1,12 +1,133 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Detail() {
     return (
-        <div>
-            Detail
-        </div>
+        <Container>
+            <Background>
+                <img src="images/wallpaperflare.com_wallpaper.jpg"/>
+            </Background>
+            <ImageTitle>
+                <img src="images/moana.png" />
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+                    <img src="images/play-icon-black.png"/>
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>
+                    <img src="images/play-icon-white.png"/>
+                    <span>Trailer</span>
+                </TrailerButton>
+                <AddButton>
+                    <span>+</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="images/group-icon.png" />
+                </GroupWatchButton>
+            </Controls>
+            <SubTitle>
+                2018 • 7m • Family, Fantasy, Kids, Animation
+            </SubTitle>
+            <Description> 
+            An adventurous teenager sails out on a daring mission to save her people. During her journey, Moana meets the once-mighty demigod Maui, who guides her in her quest to become a master wayfinder. Together, they sail across the open ocean on an action-packed voyage, encountering enormous monsters and impossible odds.
+            </Description>
+        </Container>
     )
 }
 
 export default Detail
-Detail
+
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+`
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0.3;
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: 35% 30%;
+    }
+`
+const ImageTitle = styled.div`
+    height: 30vh;
+    min-height: 170px;
+    width: 35vw;
+    min-width: 200px;
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+`
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+`
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    margin-right: 22px;
+    padding: 0px 24px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb(249,249,249);
+    border: none;
+    letter-spacing: 1.5px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: rgb(198,198,198);
+    }
+`
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0,0,0,0.3);
+    border: 1px solid rgb(249,249,249);
+    color: rgb(249,249,249);
+    text-transform: uppercase;
+`
+const AddButton = styled.button`
+    height: 44px;
+    width: 44px;
+    border-radius: 50%;
+    background: rgba(0, 0,0, 0.6);
+    border: 1px solid white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    margin-right: 16px; 
+    span{
+        font-size: 30px;
+        color: white;
+    }   
+    
+`
+const GroupWatchButton = styled(AddButton)`
+    margin-right: 0;
+    background: rgb(0, 0,0);
+`
+const SubTitle = styled.div` 
+    color: rgb(249,249,249);
+    font-size: 15px;
+    min-height: 20px;
+    margin-top: 26px;
+`
+const Description = styled.div`
+    max-width: 700px;
+    line-height: 1.4;
+    font-size: 20px;
+    margin-top: 16px;
+    color: rgb(249,249,249);
+`
